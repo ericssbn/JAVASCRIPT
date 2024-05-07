0fc65f1d33b53dea4c87f7ce9a09096b53d6cf79
ericssbn <ericsieben@outlook.com>
2024-05-07 20:31:29 -0300

    INTRODUÇÃO DE BIT E CHF

--- scripts.js
+++ scripts.js
@@ -15,13 +15,33 @@ function convertValues() {
             style: "currency",
             currency: "USD"
         }).format(convertedValue);
-    } else if (currencySelect.value === "EUR") {
+    }
+
+      if (currencySelect.value === "EUR") {
         const euroToday = 6.20; // Taxa de câmbio do Euro
         convertedValue = inputCurrencyValue / euroToday; // Conversão para Euro
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "EUR"
         }).format(convertedValue);
+    }
+
+     if (currencySelect.VALUE === "BIT") {
+        const bitcoinToday = 316115.89;
+        convertValue = inputCurrencyValue / bitcoinToday;
+        currencyValueConverted.innerHTML = new Intl.NumberFormat ("en-US", {
+            style: "currency",
+            currency: "BIT"
+        })
+    }
+
+    else if (currencySelect.VALUE === "CHF") {
+        const francoToday = 5.58;
+        convertValue = inputCurrencyValue / francoToday;
+        currencyValueConverted.innerHTML = new Intl.NumberFormat ("en-CHF", {
+            style: "currency",
+            currency: "CHF"
+        })
     }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -33,7 +53,7 @@ function convertValues() {
 
 function changeCurrency(){
 const currencyName = document.getElementById("currency-name")
-const currencyImage = document.querySelector("currency-img")
+const currencyImage = document.querySelector(".currency-img")
 
 if (currencySelect.value == "USD"){
     currencyName.innerHTML = "Dólar Americano"
@@ -42,7 +62,17 @@ currencyImage.src= "./assets/Dolar.png"
 
 if (currencySelect.value == "EUR"){
     currencyName.innerHTML = "Euro"
-    currencyImage.src= "./assets/Euro.png"
+    currencyImage.src = "./assets/Euro.png"
+}
+
+if (currencySelect.value == "BIT"){
+    currencyName.innerHTML = "Bitcoin"
+    currencyImage.src = "./assets/bitcoin.png"
+}
+
+if (currencySelect.value == "CHF"){
+    currencyName.innerHTML = "Franco Suiço"
+    currencyImage.src = "./assets/suiça.png"
 }
 
 }
